112d88624c4641e1123d08eca072b73bfa94329f
amanmi625 <155220475+amanmi625@users.noreply.github.com>
2024-06-25 20:44:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ getButton = document.querySelector("form button");
 
 for (let i = 0; i < dropList.length; i++) {
     for(let currency_code in country_list){
-        let selected = i == 0 ? currency_code == "USD" ? "selected" : "" : currency_code == "NPR" ? "selected" : "";
+        let selected = i == 0 ? currency_code == "USD" ? "selected" : "" : currency_code == "INR" ? "selected" : "";
         let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
         dropList[i].insertAdjacentHTML("beforeend", optionTag);
     }
